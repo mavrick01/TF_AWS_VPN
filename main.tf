b30fdba50c5d419bda64eb5f4f18da3eceb00572
Mavrick01 <mgollop@hotmail.com>
2022-02-09 16:53:11 +1100

    VPN setup

---
+++ main.tf
@@ -0,0 +1,72 @@
+provider "aws" {
+  region = var.aws_region
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile = var.aws_profile
+  }
+
+# Create a VPC
+resource "aws_vpc" "vpc1" {
+    cidr_block = var.aws_vpc_cidr
+
+    tags = {
+        Name = var.aws_vpc_name
+    }
+}
+
+# Create the Subnet
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = var.aws_vpc_sn_cidr
+
+  tags = {
+    Name = var.aws_vpc_sn_name
+  }
+}
+
+# Create a route of the specified network (e.g. 0.0.0.0/0 to the VPN Gateway)
+resource "aws_route_table" "sn1-rt" {
+  vpc_id = aws_vpc.vpc1.id
+
+  route {
+    cidr_block = var.aws_rt_cidr
+    gateway_id = aws_vpn_gateway.vpn_gw1.id
+  }
+
+  tags = {
+    Name = var.aws_rt_name
+  }
+}
+
+# Set up the VPN connection. This ties the VPN Gateway and Custome Gateway together
+# We set up 2 tunnels for redundnat connections and we are setting the system up for IPSec. Currently it is only static routes
+resource "aws_vpn_connection" "vpn1" {
+  vpn_gateway_id      = aws_vpn_gateway.vpn_gw1.id
+  customer_gateway_id = aws_customer_gateway.cust_gw1.id
+  type                = "ipsec.1"
+  static_routes_only  = true
+  tunnel1_preshared_key = var.tunnel1_key
+  tunnel1_inside_cidr = var.tunnel1_cidr
+  tunnel2_preshared_key = var.tunnel2_key
+  tunnel2_inside_cidr = var.tunnel2_cidr
+}
+# Set up the customer gateway, not much here except to define the BGP ASN and Router ID 
+
+  resource "aws_customer_gateway" "cust_gw1" {
+  bgp_asn    = var.cust_gw_asn
+  ip_address = var.cust_gw_ip
+  type       = "ipsec.1"
+
+  tags = {
+    Name = var.cust_gw_name
+  }
+}
+
+# Set up the VPN gateway, not much here.
+resource "aws_vpn_gateway" "vpn_gw1" {
+  vpc_id = aws_vpc.vpc1.id
+
+  tags = {
+    Name = var.vpn_gw_name
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,91 @@
+# Variable declarations
+variable "aws_region" {
+  description = "Region to operate in"
+  type = string
+  default = "ap-southeast-2"
+}
+
+variable "aws_profile" {
+  description = "AWS Account for this to work in, if not set via export TF_VAR_aws_profile=<XXX>"
+  type = string
+}
+
+variable "aws_vpc_name" {
+  description = "Name of the VPC"
+  type = string
+  default = "VPC1"
+}
+
+variable "aws_vpc_cidr" {
+  description = "Set a specific CIDR for the VPC in aws_vpc_name"
+  type = string
+  default = "172.33.0.0/16"
+}
+
+variable "aws_vpc_sn_name" {
+  description = "Name of the VPC"
+  type = string
+  default = "Subnet1"
+}
+
+variable "aws_vpc_sn_cidr" {
+  description = "Set a specific subnet for the VPC in aws_vpc_name"
+  type = string
+  default = "172.33.0.0/20"
+}
+variable "aws_rt_name" {
+  description = "name of the route on the other end of the gateway links"
+  type = string
+  default = "Route1"
+}
+
+variable "aws_rt_cidr" {
+  description = "Route on the other end of the gateway links"
+  type = string
+  default = "10.3.3.0/24"
+}
+
+variable "tunnel1_key" {
+  description = "Pre-shared key for VPN tunnel1"
+  type = string
+  default = "myspecialkey"
+}
+
+variable "tunnel1_cidr" {
+  description = "network subnet for for VPN tunnel1"
+  type = string
+  default = "169.254.10.8/30"
+}
+
+variable "tunnel2_key" {
+  description = "Pre-shared key for VPN tunnel2"
+  type = string
+  default = "myspecialkey"
+}
+
+variable "tunnel2_cidr" {
+  description = "network subnet for for VPN tunnel2"
+  type = string
+  default = "169.254.20.8/30"
+}
+
+variable "cust_gw_name" {
+  description = "name of the customer gateway"
+  default = "customer_gateway_1"
+}
+
+variable "cust_gw_asn" {
+  description = "ASN to use for the customer gateway"
+  default = 65000
+}
+
+variable "cust_gw_ip" {
+  description = "IP Lookback for the customer gateway"
+  type = string
+  default = "120.21.30.253"
+}
+
+variable "vpn_gw_name" {
+  description = "name of the VPN gateway"
+  default = "vpn_gateway_1"
+}
